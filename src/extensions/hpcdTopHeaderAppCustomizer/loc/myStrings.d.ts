35c5b3eed5d9976b824197f401802f78148168d1
Duygu Kayabasi <kayabasiduygu@gmail.com>
2023-01-16 23:46:41 +0300

    created

---
+++ src/extensions/hpcdTopHeaderAppCustomizer/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IHpcdTopHeaderAppCustomizerApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'HpcdTopHeaderAppCustomizerApplicationCustomizerStrings' {
+  const strings: IHpcdTopHeaderAppCustomizerApplicationCustomizerStrings;
+  export = strings;
+}
